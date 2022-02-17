6f19e88190908f7ae05a6fc30ab119347a94e518
micrxn <94928232+micrxn@users.noreply.github.com>
2022-02-17 16:14:56 +0100

    Add files via upload

--- functions pt1/index.js
+++ functions pt1/index.js
@@ -27,3 +27,14 @@ let resultFunction = (number) => {
 }
 
 resultFunction(2)
+
+let colorText = document.getElementById("colorText");
+let textSize = document.getElementById("textSize");
+let btn = document.getElementById("apply");
+let result = document.getElementById("result")
+let textColor = (element, color) => element.style.color = color.value
+let textSizeFunc = (element, textSize) => element.style.fontSize = // ne znam koe properti se koristi za font
+btn.addEventListener("click", () => {
+    textColor(result, colorText)
+    textSizeFunc(result, textSize)
+})
